fbb4130f6f037f8b9942917e329bd4d073a78f2b
park990 <rhkgkrwk2008@gmail.com>
2025-11-21 21:41:43 +0900

    row_and_column Study

---
+++ row_and_column/lib/screen/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:row_and_column/const/colors.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: colors.map(
+              (e)=>Container(
+                height: 50.0,
+                width:50.0,
+                color:e,
+              )
+             ).toList(),
+            
+          ),
+        ),
+      ),
+    );
+  }
+}

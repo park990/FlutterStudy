35fb10e26af04671782a7e632e2ff935638ce7fe
park990 <rhkgkrwk2008@gmail.com>
2025-11-26 21:34:36 +0900

    VideoPlayer padLeft

--- vid_player/lib/screen/home_screen.dart
+++ vid_player/lib/screen/home_screen.dart
@@ -145,8 +145,14 @@ class _VideoPlayerState extends State<_VideoPlayer> {
       )
     );
     await videoPlayerController.initialize();
+
+    videoPlayerController.addListener((){
+      setState(() {
+        
+      });
+    });
+
     setState(() {
-      
     });
   }
 
@@ -166,17 +172,49 @@ class _VideoPlayerState extends State<_VideoPlayer> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   IconButton(
-                    onPressed: (){},
+                    onPressed: (){
+                      final currentPosition = videoPlayerController.value.position;
+                      
+                      Duration position = Duration();
+                      
+                      if(currentPosition.inSeconds>3){
+                        position = currentPosition - Duration(seconds: 3);
+                      }
+                      videoPlayerController.seekTo(position);
+                    },
                     color: Colors.white,
                     icon: Icon(Icons.rotate_left),
                     ),
                   IconButton(
-                    onPressed: (){},
+                    onPressed: (){
+                      if(videoPlayerController.value.isPlaying){
+                        videoPlayerController.pause();
+                      }else{
+                        videoPlayerController.play();
+                      }
+                      setState(() {
+                        
+                      });
+                    },
                     color: Colors.white,
-                    icon: Icon(Icons.play_arrow),
+                    icon: Icon(
+                      videoPlayerController.value.isPlaying
+                      ? Icons.pause 
+                      : Icons.play_arrow
+                      ),
                     ),
                   IconButton(
-                    onPressed: (){},
+                    onPressed: (){
+                      final currentPosition = videoPlayerController.value.position;
+                      final maxPosition = videoPlayerController.value.duration;
+
+                      Duration position = maxPosition;
+                      if((maxPosition - Duration(seconds: 3)).inSeconds>currentPosition.inSeconds){
+                        position = currentPosition + Duration(seconds: 3);
+                      }
+
+                      videoPlayerController.seekTo(position);
+                    },
                     color: Colors.white,
                     icon: Icon(Icons.rotate_right),
                     ),
@@ -187,9 +225,44 @@ class _VideoPlayerState extends State<_VideoPlayer> {
               bottom: 0,
               left: 0,
               right: 0,
-              child: Slider(
-               value: 0,
-               onChanged:(double val){},
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Row(
+                  children: [
+                    Text(
+                      // 0 -> 00
+                      // 1 -> 01
+                      '${videoPlayerController
+                      .value
+                      .position
+                      .inMinutes.toString()
+                        .padLeft(2,'0')}:${(videoPlayerController.value.position.inSeconds % 60).toString().padLeft(2,'0')}',
+                      style: TextStyle(
+                        color:Colors.white,
+                      ),
+                    ),
+                    Expanded(
+                      child: Slider(
+                        value: videoPlayerController.value.position.inSeconds.toDouble(),
+                        max: videoPlayerController.value.duration.inSeconds.toDouble(),
+                        onChanged:(double val){}, 
+                        ),
+                    ),
+                    Text(
+                      '${videoPlayerController
+                      .value
+                      .duration
+                      .inMinutes.toString()
+                        .padLeft(2,'0')}:${(videoPlayerController.value.duration.inSeconds % 60).toString()
+                        .padLeft(2,'0')}',
+                      style: TextStyle(
+                        color:Colors.white,
+                      ),
+                    ),
+                  ]
+                ),
               ),
             ),
             Positioned(

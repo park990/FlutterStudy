d99271c44ba14a161d2c9b306b537a888b002454
park990 <rhkgkrwk2008@gmail.com>
2025-12-04 12:57:57 +0900

    Reorderable List View

--- scrollable_widgets/lib/screen/home_screen.dart
+++ scrollable_widgets/lib/screen/home_screen.dart
@@ -2,16 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:scrollable_widgets/layout/main_layout.dart';
 import 'package:scrollable_widgets/screen/grid_view_screen.dart';
 import 'package:scrollable_widgets/screen/list_view_screen.dart';
+import 'package:scrollable_widgets/screen/reorderable_list_view_screen.dart';
 import 'package:scrollable_widgets/screen/single_child_scroll.dart';
 
 class ScreenModel {
   final WidgetBuilder builder;
   final String name;
 
-  ScreenModel({
-    required this.builder,
-    required this.name,
-  });
+  ScreenModel({required this.builder, required this.name});
 }
 
 class HomeScreen extends StatelessWidget {
@@ -25,8 +23,12 @@ class HomeScreen extends StatelessWidget {
       name: 'ListViewScreenH',
     ),
     ScreenModel(
-      builder: (_)=>GridViewScreen(),
-      name: 'GridViewScreenH',)
+      builder: (_) => GridViewScreen(),
+      name: 'GridViewScreenH',
+    ),
+    ScreenModel(
+    builder: (_)=>ReorderableListViewScreen(),
+    name: 'ReorderableListviewScreenH'),
   ];
 
   HomeScreen({super.key});
@@ -36,24 +38,24 @@ class HomeScreen extends StatelessWidget {
     return MainLayout(
       title: 'Home',
       body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16,
-        ),
-        child: Column(
-          mainAxisAlignment:
-              MainAxisAlignment.center,
-          crossAxisAlignment:
-              CrossAxisAlignment.stretch,
-          children: screens.map(
-            (screen) => ElevatedButton(
-              onPressed: (){
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder:screen.builder)
-                );
-              },
-              child: Text(screen.name)
-            )
-          ).toList()
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: screens
+                .map(
+                  (screen) => ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: screen.builder),
+                      );
+                    },
+                    child: Text(screen.name),
+                  ),
+                )
+                .toList(),
+          ),
         ),
       ),
     );
